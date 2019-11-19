42c362f846adcb8930b4da39450c2166c2b26026
wwarriner <wwarriner@gmail.com>
2019-11-19 17:40:09 -0600

    reverted to original unet

--- unet.py
+++ unet.py
@@ -91,10 +91,40 @@ def upscale(bottom, down_convs, depth):
 def build_unet(input_shape, levels):
     DEPTH = 32
     inputs = Input(input_shape)
-    convs, pools = downscale(inputs, DEPTH, levels)
-    bottom = bottom_out(pools[-1], DEPTH, levels)
-    convs, _ = upscale(bottom, convs, DEPTH)
-    act = activate(convs[-1])
+    # convs, pools = downscale(inputs, DEPTH, levels)
+    # bottom = bottom_out(pools[-1], DEPTH, levels)
+    # convs, _ = upscale(bottom, convs, DEPTH)
+    # act = activate(convs[-1])
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(inputs)
+    conv1 = Dropout(0.2)(conv1)
+    conv1 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv1)
+    pool1 = MaxPooling2D((2, 2))(conv1)
+    #
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(pool1)
+    conv2 = Dropout(0.2)(conv2)
+    conv2 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv2)
+    pool2 = MaxPooling2D((2, 2))(conv2)
+    #
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(pool2)
+    conv3 = Dropout(0.2)(conv3)
+    conv3 = Conv2D(128, (3, 3), activation="relu", padding="same")(conv3)
+
+    up1 = UpSampling2D(size=(2, 2))(conv3)
+    up1 = concatenate([conv2, up1])
+    conv4 = Conv2D(64, (3, 3), activation="relu", padding="same")(up1)
+    conv4 = Dropout(0.2)(conv4)
+    conv4 = Conv2D(64, (3, 3), activation="relu", padding="same")(conv4)
+    #
+    up2 = UpSampling2D(size=(2, 2))(conv4)
+    up2 = concatenate([conv1, up2])
+    conv5 = Conv2D(32, (3, 3), activation="relu", padding="same")(up2)
+    conv5 = Dropout(0.2)(conv5)
+    conv5 = Conv2D(32, (3, 3), activation="relu", padding="same")(conv5)
+    #
+    conv6 = Conv2D(2, (1, 1), activation="relu", padding="same")(conv5)
+    ############
+    act = Activation("softmax")(conv6)
+
     model = Model(inputs=inputs, outputs=act)
     # TODO figure out why metrics aren't working
     model.compile(
